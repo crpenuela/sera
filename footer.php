9592d82831814798c261a78c46ce73af177f503e
crpenuela <66219291+crpenuela@users.noreply.github.com>
2020-06-21 15:42:51 -0500

    footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="clearfix"> </div>
+</div>
+<div class="copy">
+    <p>Copyright &copy; 2017 | Diseñado por <a href="https://www.facebook.com/fenixSasLaPlata/" target="_blank">Fénix S.A.S.</a></p>
+</div>
